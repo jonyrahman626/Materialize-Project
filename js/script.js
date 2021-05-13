2f74dbfa6db7ce044760d4c5f67185cd0f9bf35b
jonyrahman626 <jonyrahman15@gmail.com>
2021-05-13 22:35:15 +0600

    First

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(function () {
+    // Side menu code here
+    $('.sidenav').sidenav();
+
+    // Slider code start here
+    $('.banner_item').carousel({
+        fullWidth: true,
+        indicators: true,
+    });
+});
